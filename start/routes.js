3bfdc6c6cec8b0d63b781223d6e072ace943739b
Kalil Kabiam Vasconcelos Junior <kalil_jr@hotmail.com>
2020-03-04 21:19:57 -0300

    tentativa de deploy

--- start/routes.js
+++ start/routes.js
@@ -25,6 +25,29 @@ const authenticated = Route.group(() => {
   
   Route.get('/users','UserController.index');
   Route.post('/users','UserController.store');
+  Route.put('/users/pwd/:id', 'UserController.changePassword')
+  Route.put('/users/:id','UserController.update');
+  Route.delete('/users/delete/:id','UserController.delete');
+
+  Route.get('/clientes','ClienteController.list');
+  Route.post('/clientes','ClienteController.register');
+  Route.get('/clientes/:id','ClienteController.get');
+  Route.put('/clientes/:id','ClienteController.update');
+  Route.delete('/clientes/:id','ClienteController.delete');
+
+
+  Route.get('/parceiros','ParceiroController.list');
+  Route.post('/parceiros','ParceiroController.register');
+  Route.get('/parceiros/:id','ParceiroController.get');
+  Route.put('/parceiros/:id','ParceiroController.update');
+  Route.delete('/parceiros/:id','ParceiroController.delete');
+
+  Route.get('/chamados','ChamadoController.list');
+  Route.post('/chamados','ChamadoController.register');
+  Route.get('/chamados/:id','ChamadoController.get');
+  Route.put('/chamados/:id','ChamadoController.update');
+  Route.delete('/chamados/:id','ChamadoController.delete');
+  Route.get('/previsao/chamados','ChamadoController.previsao');
 
 
 }).prefix('api').middleware('auth')
@@ -35,6 +58,8 @@ const non_authenticated = Route.group(() => {
   Route.post('/users/resetpwd', 'UserController.ask_reset_pwd')
   Route.get('/reset-password/:id', 'UserController.reset_pwd_hash')
   Route.post('sessions/login', 'SessionController.create')
+  Route.post('sessions/clientes/login', 'SessionController.create_cliente_login')
 
+  Route.get('/teste','SessionController.teste');
   
 }).prefix('api')
